cca53369e56f6df175f8ccf2190296adfde2605c
DiracSpace <www.ralphlaurenpolo@gmail.com>
2020-10-19 02:47:04 -0500

    Added HuffmanTree and HuffmanNode classes

--- WinFormApplications/HuffmanCompressionAlgorithm/Form1.Designer.cs
+++ WinFormApplications/HuffmanCompressionAlgorithm/Form1.Designer.cs
@@ -32,13 +32,15 @@
             this.lvInformation = new System.Windows.Forms.ListView();
             this.chLetter = new System.Windows.Forms.ColumnHeader();
             this.chFrequency = new System.Windows.Forms.ColumnHeader();
+            this.txtTextInput = new System.Windows.Forms.TextBox();
+            this.txtOutputText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // startProgramBtn
             // 
-            this.startProgramBtn.Location = new System.Drawing.Point(220, 102);
+            this.startProgramBtn.Location = new System.Drawing.Point(651, 12);
             this.startProgramBtn.Name = "startProgramBtn";
-            this.startProgramBtn.Size = new System.Drawing.Size(75, 23);
+            this.startProgramBtn.Size = new System.Drawing.Size(106, 28);
             this.startProgramBtn.TabIndex = 1;
             this.startProgramBtn.Text = "Start";
             this.startProgramBtn.UseVisualStyleBackColor = true;
@@ -50,7 +52,7 @@
             this.chLetter,
             this.chFrequency});
             this.lvInformation.HideSelection = false;
-            this.lvInformation.Location = new System.Drawing.Point(13, 13);
+            this.lvInformation.Location = new System.Drawing.Point(12, 82);
             this.lvInformation.Name = "lvInformation";
             this.lvInformation.Size = new System.Drawing.Size(201, 381);
             this.lvInformation.TabIndex = 2;
@@ -68,17 +70,36 @@
             this.chFrequency.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.chFrequency.Width = 90;
             // 
+            // txtTextInput
+            // 
+            this.txtTextInput.Location = new System.Drawing.Point(220, 82);
+            this.txtTextInput.Multiline = true;
+            this.txtTextInput.Name = "txtTextInput";
+            this.txtTextInput.Size = new System.Drawing.Size(537, 177);
+            this.txtTextInput.TabIndex = 3;
+            // 
+            // txtOutputText
+            // 
+            this.txtOutputText.Location = new System.Drawing.Point(220, 265);
+            this.txtOutputText.Multiline = true;
+            this.txtOutputText.Name = "txtOutputText";
+            this.txtOutputText.Size = new System.Drawing.Size(537, 198);
+            this.txtOutputText.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(307, 406);
+            this.ClientSize = new System.Drawing.Size(769, 475);
+            this.Controls.Add(this.txtOutputText);
+            this.Controls.Add(this.txtTextInput);
             this.Controls.Add(this.lvInformation);
             this.Controls.Add(this.startProgramBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,6 +108,8 @@
         private System.Windows.Forms.ListView lvInformation;
         private System.Windows.Forms.ColumnHeader chLetter;
         private System.Windows.Forms.ColumnHeader chFrequency;
+        private System.Windows.Forms.TextBox txtTextInput;
+        private System.Windows.Forms.TextBox txtOutputText;
     }
 }
 
